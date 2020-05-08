959c56261c77ba670a63ff228f10106521285eb0
Shreyanshsg788 <30348534+Shreyanshsg788@users.noreply.github.com>
2020-05-08 19:25:17 +0530

    disabled_keys

--- vars.tf
+++ vars.tf
@@ -3,10 +3,10 @@
 
 variable "access_key" {
     description = "My AWS Access Key"
-    default = "AKIAQJYHBAO3JKUEFTMM"
+    default = "xxxx"
 }
 
 variable "secret_key" {
     description = "My AWS Secret Key"
-    default = "QvwJC+w8vk+f9dQ+DlPb4Pvny/0hQg/6wGsHYIXg"
+    default = "xxxx"
 }
